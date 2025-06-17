a5ca314ce005f6b2285aef4dc3d465d76f558178
Tomohiko Takahashi <takahashi@tomohiko.io>
2025-06-18 00:23:00 +0900

    新しいスクリプトverup.shを追加し、package.jsonのバージョンを自動的にインクリメントする処理を実装

---
+++ verup.sh
@@ -0,0 +1,1 @@
+find tools -name "package.json" -exec sh -c 'echo "Processing: $1"; jq ".version = (.version | split(\".\") | .[2] = (.[2] | tonumber + 1 | tostring) | join(\".\"))" "$1" > "$1.tmp" && mv "$1.tmp" "$1"' _ {} \;
