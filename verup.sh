d8b7ad99a7db7c0d01fdf843ba4bfeefc203a574
Takahashi Tomohiko <takahashi@tomohiko.io>
2025-06-22 20:16:34 +0900

    debug

--- verup.sh
+++ verup.sh
@@ -1,1 +1,2 @@
-find tools -name "package.json" -exec sh -c 'echo "Processing: $1"; jq ".version = (.version | split(\".\") | .[2] = (.[2] | tonumber + 1 | tostring) | join(\".\"))" "$1" > "$1.tmp" && mv "$1.tmp" "$1"' _ {} \;
+# Process package.json files in tools subdirectories only (not nested)
+find tools -maxdepth 2 -mindepth 2 -name "package.json" -exec sh -c 'echo "Processing: $1"; jq ".version = (.version | split(\".\") | .[2] = (.[2] | tonumber + 1 | tostring) | join(\".\"))" "$1" > "$1.tmp" && mv "$1.tmp" "$1"' _ {} \;
