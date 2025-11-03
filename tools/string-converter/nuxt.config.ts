e90530312ca6dae8b70015abb90e8dd94fcf0ce3
Takahashi Tomohiko <takahashi@tomohiko.io>
2025-11-04 08:51:58 +0900

    CSPで自分のスクリプトも読み込めなくなっていた問題を解消 (#50) (#51)

--- tools/string-converter/nuxt.config.ts
+++ tools/string-converter/nuxt.config.ts
@@ -18,7 +18,7 @@ export default defineNuxtConfig({
 ,
         {
           'http-equiv': 'Content-Security-Policy',
-          content: "default-src 'self'; script-src 'self' 'wasm-unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob: https:; font-src 'self' data:; connect-src 'self' https:; frame-ancestors 'none'; base-uri 'self'; form-action 'self'; object-src 'none';"
+          content: "default-src 'self'; script-src 'self' 'wasm-unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob: https:; font-src 'self' data:; connect-src 'self' https:; frame-ancestors 'none'; base-uri 'self'; form-action 'self'; object-src 'none';"
         }
       ]
     }
