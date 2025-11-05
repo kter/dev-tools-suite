7a9185e797037169ef98090240dc49b6c6fc399e
Takahashi Tomohiko <takahashi@tomohiko.io>
2025-11-05 09:10:58 +0900

    修正

--- infrastructure/cdk/lib/dev-tools-stack.ts
+++ infrastructure/cdk/lib/dev-tools-stack.ts
@@ -48,7 +48,7 @@ export class DevToolsStack extends cdk.Stack {
             "script-src 'self' 'wasm-unsafe-eval' 'unsafe-inline' storage.ko-fi.com ko-fi.com www.googletagmanager.com fonts.gstatic.com",
             "style-src 'self' 'unsafe-inline' storage.ko-fi.com ko-fi.com fonts.googleapis.com fonts.gstatic.com",
             "img-src 'self' data: blob: https:",
-            "font-src 'self' data:",
+            "font-src 'self' data: fonts.gstatic.com",
             "connect-src 'self' https:",
             "frame-ancestors 'none'",
             "base-uri 'self'",
