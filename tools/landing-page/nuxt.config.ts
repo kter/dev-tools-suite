84028a05440e1acc58a197ce9c2a89f607f9d23c
Takahashi Tomohiko <takahashi@tomohiko.io>
2025-11-05 09:20:34 +0900

    修正

--- tools/landing-page/nuxt.config.ts
+++ tools/landing-page/nuxt.config.ts
@@ -14,7 +14,7 @@ export default defineNuxtConfig({
 ,
         {
           'http-equiv': 'Content-Security-Policy',
-          content: "default-src 'self'; script-src 'self' 'wasm-unsafe-eval' 'unsafe-inline' storage.ko-fi.com ko-fi.com www.googletagmanager.com fonts.gstatic.com; style-src 'self' 'unsafe-inline' storage.ko-fi.com ko-fi.com fonts.googleapis.com fonts.gstatic.com; img-src 'self' data: blob: https:; font-src 'self' data: fonts.gstatic.com; connect-src 'self' https:; frame-ancestors 'none'; base-uri 'self'; form-action 'self'; object-src 'none';"
+          content: "default-src 'self' ko-fi.com; script-src 'self' 'wasm-unsafe-eval' 'unsafe-inline' storage.ko-fi.com ko-fi.com www.googletagmanager.com fonts.gstatic.com; style-src 'self' 'unsafe-inline' storage.ko-fi.com ko-fi.com fonts.googleapis.com fonts.gstatic.com; img-src 'self' data: blob: https:; font-src 'self' data: fonts.gstatic.com; connect-src 'self' https:; frame-ancestors 'none'; base-uri 'self'; form-action 'self'; object-src 'none';"
         }
       ]
     }
